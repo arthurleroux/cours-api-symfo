f15ebddca9842e1068de1fae7c775603adf53903
Arthur Leroux <arthur_leroux@hotmail.fr>
2019-03-18 16:48:45 +0100

    Ehh

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->encoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        for($i=0; $i>10; $i++) {
+            $product = new Product();
+            $product->setName('Product' . $i);
+            $product->setPrice(rand(10,50));
+            $product->setSku('product-' . $i);
+
+            $manager->persist($product);
+        }
+
+        $user = new User();
+        $user->setEmail('arthur@gmail.com');
+        $user->setRoles([
+            'ROLE_ADMIN'
+        ]);
+
+        $user->setPassword($this->encoder->encodePassword($user, 'password'));
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
